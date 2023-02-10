f61c1cc0c2b50054b66ea83c92b9793101cbe872
19ug5 <Saikrishna@307979>
2023-02-10 13:36:25 +0530

    Add project files.

---
+++ VendingMachine/CollectCoins.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingMachine
+{
+    internal class CollectCoins
+    {
+       private double CurrentAmt { get; set; } 
+       
+       public void TakeCoins()
+       {
+            Console.WriteLine("Please insert valid coin : ");
+            double Input = Console.Read();
+            if (IsCoinValid(Input))
+            {
+
+            }
+       }
+
+        public void DisplayAmount()
+        {
+            Console.WriteLine("Current Amount is : {0}",CurrentAmt);
+        }
+
+        public bool IsCoinValid(double coin)
+        {
+            return true;
+        }
+    }
+}
